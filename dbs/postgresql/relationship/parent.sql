2a4ad8988f3c538505d4574b65f74e8cbb99fbd2
Arthur-codeX <85581425+Arthur-codeX@users.noreply.github.com>
2025-08-19 12:32:48 +0300

    completed relationships

--- dbs/postgresql/relationship/parent.sql
+++ dbs/postgresql/relationship/parent.sql
@@ -8,13 +8,13 @@
 -- );
 
 --DROP TABLE IF EXISTS parent_address ;
-CREATE TABLE IF NOT EXISTS parent_address(
-    id BIGSERIAL PRIMARY KEY,
-    parent_email VARCHAR(250) REFERENCES parent(email) ,
-    county_name VARCHAR(250) ,
-    town VARCHAR(250),
-    longitude REAL,
-    latitude REAL,
-    house_no VARCHAR(200)
-);
+-- CREATE TABLE IF NOT EXISTS parent_address(
+--     id BIGSERIAL PRIMARY KEY,
+--     parent_email VARCHAR(250) REFERENCES parent(email) ,
+--     county_name VARCHAR(250) ,
+--     town VARCHAR(250),
+--     longitude REAL,
+--     latitude REAL,
+--     house_no VARCHAR(200)
+-- );
 
