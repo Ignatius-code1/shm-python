804ceadd0f3dc1897fc1514cd38bcc3c5912dd30
Arthur-codeX <daniel.karanja239@gmail.com>
2025-08-22 10:24:27 +0300

    added models

--- checkers/app/db.py
+++ checkers/app/db.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base,sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -7,4 +10,23 @@ load_dotenv()
 
 drivername=os.getenv("drivername")
 
-print(drivername)
+db_credentials={
+    "drivername":os.getenv("drivername"),
+    "host":os.getenv("host"),
+    "username":os.getenv("username"),
+    "password":os.getenv("password"),
+    "port":os.getenv("port"),
+    "database":os.getenv("database")
+}
+
+
+#BUILD URL
+DATABASE_URL=URL.create(**db_credentials)
+
+engine=create_engine(DATABASE_URL,echo=True,future=True)
+
+
+SessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
+
+#BASE
+Base=declarative_base()
